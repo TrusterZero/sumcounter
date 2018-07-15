b8c616c70a6f0a7876bf1baf46b35c1f5f02fda2
Jurmen Hughes <jurmen1992@gmail.com>
2018-07-15 16:03:46 +0200

    Added solution for absent champion data

--- SC/riot.js
+++ SC/riot.js
@@ -1,7 +1,10 @@
 const request = require("request");
 const fs = require("fs")
-const TempApiKey = "RGAPI-0fe2f7fc-d55e-4e89-ad8e-42c33babc977";
+const TEMP_API_KEY = "RGAPI-0fe2f7fc-d55e-4e89-ad8e-42c33babc977";
+const COSMIC_INSIGHT_ID = 8347 //the cosmic insight rune reduces the cooldown of summonerspells
+const MS_IN_DAY = 86400000
 let ex = module.exports;
+
 
 class Champion {
   constructor(key, name, image) {
@@ -22,7 +25,7 @@ class Spell {
 headers = {
   "Origin": "https://developer.riotgames.com",
   "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-  "X-Riot-Token": `${TempApiKey}`,
+  "X-Riot-Token": `${TEMP_API_KEY}`,
   "Accept-Language": "nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7",
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
 }
@@ -38,58 +41,59 @@ let getUser = ex.getUser = (userName) => {
   })
 }
 
-//Gets the latest championlist and saves is it ./champions.json
-//will be changed to UpdateLocalData 
-
 updateLocalFile = (type) => {
   let endpoint;
   let path;
   if (type == "spells") {
     endpoint = "summoners.json"
     path = "./spells.json"
-  } else if (type == "champion") {
+  } else if (type == "champions") {
     endpoint = "champions.json"
     path = "./champions.json"
   }
-  request.get({
-    url: `http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/${endpoint}`,
-    headers: headers
-  }, (err, res) => {
-    newData = JSON.parse(res["body"])
 
-    let localData = []
+  if (canBeUpdated(path)) {
+    request.get({
+      url: `http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/${endpoint}`,
+      headers: headers
+    }, (err, res) => {
+      newData = JSON.parse(res["body"])
 
-    if (type == "spells") {
-      for (spellName in newData["data"]) {
-        spell = newData["data"][spellName]
-        localData.push(new Spell(spell.key, spell.name, spell.image, spell.cooldown))
+      let localData = []
+
+      if (type == "spells") {
+        for (spellName in newData["data"]) {
+          spell = newData["data"][spellName]
+          localData.push(new Spell(spell.key, spell.name, spell.image, spell.cooldown))
+        }
+      } else if (type == "champions") {
+        for (championName in newData["data"]) {
+          champion = newData["data"][championName]
+          localData.push(new Champion(champion.key, champion.name, champion.image.full))
+        }
       }
-    } else if (type == "champions") {
-      for (championName in newData["data"]) {
-        champion = newData["data"][championName]
-        localData.push(new Champion(champion.key, champion.name, champion.image.full))
-      }
-    }
-
-    //sorteren voor de binary search
-    localData.sort((a, b) => {
-      return a.key - b.key
+      //sorteren voor de binary search
+      localData.sort((a, b) => {
+        return a.key - b.key
+      })
+      fs.writeFile(path, JSON.stringify(localData), (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+      return true
     })
-
-    //TODO: stats birthdate is x aantal dagen geleden dan update en anders return false
-
-    fs.writeFile(path, JSON.stringify(localData), (err) => {
-      if (err) {
-        console.log(err)
-      }
-    })
-  })
+  }else {
+    return false
+  }
 }
 
 
 let canBeUpdated = (path) => {
-  fs.stat(path, (error, stats) => {
 
+
+  fs.stat(path, (error, stats) => {
+    return Date.now() - stats.mtime > MS_IN_DAY
   })
 }
 
@@ -97,11 +101,10 @@ getLocalData = (type, key) => {
   let path;
   if (type == "champions") {
     path = "./champions.json"
-  } else if (type = "spells") {
+  } else if (type == "spells") {
     path = "./spells.json"
   }
   console.log(path)
-
   array = JSON.parse(fs.readFileSync(path))
   return binarySearch(type, key, array)
 }
@@ -110,6 +113,14 @@ binarySearch = (type, key, array) => {
   let mid = Math.floor(array.length / 2)
   let midObject = array[mid]
   let midKey = midObject.key
+  
+  if(array.length == 1 && key != array[0].key){
+    if(updateLocalFile(type)){
+      getLocalData(type, key)
+    }else {
+      return null
+    }
+  }
   switch (true) {
     case midKey == key:
       return midObject;
@@ -118,9 +129,6 @@ binarySearch = (type, key, array) => {
     case key > midKey:
       return binarySearch(type, key, array.slice(mid, array.length))
     default:
-      updateLocalFile(type) //TODO: check wanneer champions.json voor het laatst geupdate is 
-      // al is dat minder dan een dag geleden throw error
-      getLocalData(type, key)
   }
 }
 
@@ -144,7 +152,7 @@ let AddSummonerData = (match, position) => {
     match.participants[position].champion = getLocalData("champions", summoner.championId)
     match.participants[position].sum1 = getLocalData("spells", summoner.spell1Id)
     match.participants[position].sum2 = getLocalData("spells", summoner.spell2Id)
-    if(hasCDR(summoner)){
+    if (hasCDR(summoner)) {
       match.participants[position].sum1.cooldown[0] *= 0.95
       match.participants[position].sum2.cooldown[0] *= 0.95
     }
@@ -153,11 +161,11 @@ let AddSummonerData = (match, position) => {
 }
 
 let hasCDR = (user) => {
-  COSMIC_INSIGHT_ID = 8347 //the cosmic insight rune reduces the cooldown of summonerspells
   return user.perks.perkIds.includes(COSMIC_INSIGHT_ID)
 }
 
-getMatch("zsu").then((match) => {
+getMatch("Ryone").then((match) => {
   console.log(JSON.stringify(match, undefined, 2));
 })
 
+updateLocalFile("champions")
